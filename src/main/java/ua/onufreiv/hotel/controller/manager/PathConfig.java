6360aa3833fcb0dc9ef83426b8eecffc441d60bd
Yurii Onufreiv <onufreyiv.y.e@gmail.com>
2017-01-04 14:43:08 +0200

    Paths to .jsp refactored

--- src/main/java/ua/onufreiv/hotel/controller/manager/PathConfig.java
+++ src/main/java/ua/onufreiv/hotel/controller/manager/PathConfig.java
@@ -6,14 +6,16 @@ import java.util.ResourceBundle;
  * Created by yurii on 12/27/16.
  */
 public class PathConfig {
+    private static final String BUNDLE_NAME = "paths";
+
     public static final String DATABASE_DRIVER_NAME = "DATABASE_DRIVER_NAME";
     public static final String DATABASE_URL = "DATABASE_URL";
     public static final String ERROR_PAGE_PATH = "ERROR_PAGE_PATH";
-    public static final String LOGIN_PAGE_PATH = "LOGIN_PAGE_PATH";
     public static final String MAIN_PAGE_PATH = "MAIN_PAGE_PATH";
-    public static final String REGISTER_PAGE_PATH = "REGISTER_PAGE_PATH";
-    public static final String RESERVATION_PAGE_PATH = "RESERVATION_PAGE_PATH";
-    private static final String BUNDLE_NAME = "paths";
+    public static final String LOGIN_PAGE_PATH = "LOGIN_PAGE_PATH";
+    public static final String CREATE_ACCOUNT_PAGE_PATH = "CREATE_ACCOUNT_PAGE_PATH";
+    public static final String NEW_BOOK_REQUEST_PAGE_PATH = "NEW_BOOK_REQUEST_PAGE_PATH";
+    public static final String SHOW_ALL_REQUESTS_PAGE_PATH = "SHOW_ALL_REQUESTS_PAGE_PATH";
 
     private static PathConfig instance;
 
