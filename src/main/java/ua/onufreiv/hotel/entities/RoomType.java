fc692c67e751d50bfb3601d7ea277383f0d5e934
Yurii Onufreiv <onufreyiv.y.e@gmail.com>
2017-01-04 14:50:14 +0200

    RoomType entity added

---
+++ src/main/java/ua/onufreiv/hotel/entities/RoomType.java
@@ -0,0 +1,64 @@
+package ua.onufreiv.hotel.entities;
+
+/**
+ * Created by yurii on 1/4/17.
+ */
+public class RoomType extends AbstractEntity {
+    private String type;
+    private String description;
+    private Integer price;
+    private Integer maxPerson;
+
+    public RoomType() {
+    }
+
+    public RoomType(Integer id, String type, String description, Integer price, Integer maxPerson) {
+        super(id);
+        this.type = type;
+        this.description = description;
+        this.price = price;
+        this.maxPerson = maxPerson;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Integer getMaxPerson() {
+        return maxPerson;
+    }
+
+    public void setMaxPerson(Integer maxPerson) {
+        this.maxPerson = maxPerson;
+    }
+
+    @Override
+    public String toString() {
+        return "RoomType{" +
+                ", type='" + type + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                ", maxPerson=" + maxPerson +
+                '}';
+    }
+}
