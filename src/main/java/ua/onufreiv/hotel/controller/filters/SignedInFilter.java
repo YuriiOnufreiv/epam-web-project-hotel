b0cc022ba7415b375560a1fac5e80caf7321e2c2
Yurii Onufreiv <onufreyiv.y.e@gmail.com>
2017-01-04 17:06:27 +0200

    <url-pattern> for signedInFilter changed to be global

--- src/main/java/ua/onufreiv/hotel/controller/filters/SignedInFilter.java
+++ src/main/java/ua/onufreiv/hotel/controller/filters/SignedInFilter.java
@@ -12,14 +12,15 @@ import java.io.IOException;
  * Created by yurii on 1/4/17.
  */
 public class SignedInFilter implements Filter {
+
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-
     }
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpSession session = ((HttpServletRequest) request).getSession(false);
+
         if (session == null || session.getAttribute("user") == null) {
             ((HttpServletResponse) response).sendRedirect(PathConfig.getInstance()
                     .getProperty(PathConfig.NOT_SIGNED_IN_ERROR_PAGE_PATH));
