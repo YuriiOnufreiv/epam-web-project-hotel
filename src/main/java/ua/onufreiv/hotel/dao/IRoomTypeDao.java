22ab8e27b7917fcaa3a616c7c6f5a84191884ae2
Yurii Onufreiv <onufreyiv.y.e@gmail.com>
2017-01-04 15:08:00 +0200

    getIdTypeTitleMap() method added

--- src/main/java/ua/onufreiv/hotel/dao/IRoomTypeDao.java
+++ src/main/java/ua/onufreiv/hotel/dao/IRoomTypeDao.java
@@ -2,8 +2,11 @@ package ua.onufreiv.hotel.dao;
 
 import ua.onufreiv.hotel.entities.RoomType;
 
+import java.util.Map;
+
 /**
  * Created by yurii on 1/4/17.
  */
 public interface IRoomTypeDao extends IDao<RoomType> {
+    Map<Integer, String> getIdTypeTitleMap();
 }
