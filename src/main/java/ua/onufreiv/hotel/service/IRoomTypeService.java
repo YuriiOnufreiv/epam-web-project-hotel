1884652b73bfba995b2e815d61a3d1181a5cfd0c
Yurii Onufreiv <onufreyiv.y.e@gmail.com>
2017-01-04 17:15:10 +0200

    IRoomTypeService created

---
+++ src/main/java/ua/onufreiv/hotel/service/IRoomTypeService.java
@@ -0,0 +1,10 @@
+package ua.onufreiv.hotel.service;
+
+import java.util.Map;
+
+/**
+ * Created by yurii on 1/4/17.
+ */
+public interface IRoomTypeService {
+    Map<Integer, String> getIdTypeTitleMap();
+}
