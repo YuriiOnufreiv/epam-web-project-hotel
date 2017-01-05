c8e426202d07779a30eefe58aa7786550088408f
Yurii Onufreiv <onufreyiv.y.e@gmail.com>
2017-01-05 23:11:27 +0200

    ReservedRoom entity added

---
+++ src/main/java/ua/onufreiv/hotel/entities/ReservedRoom.java
@@ -0,0 +1,55 @@
+package ua.onufreiv.hotel.entities;
+
+import java.util.Date;
+
+/**
+ * Created by yurii on 1/5/17.
+ */
+public class ReservedRoom extends AbstractEntity {
+    private Integer roomId;
+    private Date checkInDate;
+    private Date checkOutDate;
+
+    public ReservedRoom() {
+    }
+
+    public ReservedRoom(Integer id, Integer roomId, Date checkInDate, Date checkOutDate) {
+        super(id);
+        this.roomId = roomId;
+        this.checkInDate = checkInDate;
+        this.checkOutDate = checkOutDate;
+    }
+
+    public Integer getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(Integer roomId) {
+        this.roomId = roomId;
+    }
+
+    public Date getCheckInDate() {
+        return checkInDate;
+    }
+
+    public void setCheckInDate(Date checkInDate) {
+        this.checkInDate = checkInDate;
+    }
+
+    public Date getCheckOutDate() {
+        return checkOutDate;
+    }
+
+    public void setCheckOutDate(Date checkOutDate) {
+        this.checkOutDate = checkOutDate;
+    }
+
+    @Override
+    public String toString() {
+        return "ReservedRoom{" +
+                "roomId=" + roomId +
+                ", checkInDate=" + checkInDate +
+                ", checkOutDate=" + checkOutDate +
+                '}';
+    }
+}
