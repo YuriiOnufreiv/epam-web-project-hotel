19bb4f55003016a8a77c5118866927f480a4b4d8
Yurii Onufreiv <onufreyiv.y.e@gmail.com>
2017-01-05 23:07:49 +0200

    find(int id) method added

--- src/main/java/ua/onufreiv/hotel/service/IRoomTypeService.java
+++ src/main/java/ua/onufreiv/hotel/service/IRoomTypeService.java
@@ -1,10 +1,13 @@
 package ua.onufreiv.hotel.service;
 
+import ua.onufreiv.hotel.entities.RoomType;
+
 import java.util.Map;
 
 /**
  * Created by yurii on 1/4/17.
  */
 public interface IRoomTypeService {
+    RoomType find(int id);
     Map<Integer, String> getIdTypeTitleMap();
 }
--- src/main/java/ua/onufreiv/hotel/service/impl/RoomTypeService.java
+++ src/main/java/ua/onufreiv/hotel/service/impl/RoomTypeService.java
@@ -1,6 +1,7 @@
 package ua.onufreiv.hotel.service.impl;
 
 import ua.onufreiv.hotel.dao.DaoFactory;
+import ua.onufreiv.hotel.entities.RoomType;
 import ua.onufreiv.hotel.service.IRoomTypeService;
 
 import java.util.Map;
@@ -9,6 +10,11 @@ import java.util.Map;
  * Created by yurii on 1/4/17.
  */
 public class RoomTypeService implements IRoomTypeService {
+    @Override
+    public RoomType find(int id) {
+        return DaoFactory.getDAOFactory(DaoFactory.FactoryType.MYSQL_DB).getRoomTypeDAO().find(id);
+    }
+
     @Override
     public Map<Integer, String> getIdTypeTitleMap() {
         return DaoFactory.getDAOFactory(DaoFactory.FactoryType.MYSQL_DB).getRoomTypeDAO().getIdTypeTitleMap();
