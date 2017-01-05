b9e36e68ce5a1606e4e9b94ea0256e0ca676519e
Yurii Onufreiv <onufreyiv.y.e@gmail.com>
2017-01-05 20:20:49 +0200

    Room entity added

---
+++ src/main/java/ua/onufreiv/hotel/entities/Room.java
@@ -0,0 +1,42 @@
+package ua.onufreiv.hotel.entities;
+
+/**
+ * Created by yurii on 1/5/17.
+ */
+public class Room extends AbstractEntity {
+    private Integer roomTypeId;
+    private Integer number;
+
+    public Room() {
+    }
+
+    public Room(Integer id, Integer roomTypeId, Integer number) {
+        super(id);
+        this.roomTypeId = roomTypeId;
+        this.number = number;
+    }
+
+    public Integer getRoomTypeId() {
+        return roomTypeId;
+    }
+
+    public void setRoomTypeId(Integer roomTypeId) {
+        this.roomTypeId = roomTypeId;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return "Room{" +
+                ", roomTypeId=" + roomTypeId +
+                ", number=" + number +
+                '}';
+    }
+}
