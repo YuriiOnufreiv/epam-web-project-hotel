5833c56044929765e53d35a774be193dfb2d5d97
Yurii <yurii.onufreiv@gmail.com>
2017-01-14 13:22:41 +0200

    getById() method added

--- src/main/java/ua/onufreiv/hotel/service/IUserService.java
+++ src/main/java/ua/onufreiv/hotel/service/IUserService.java
@@ -7,5 +7,6 @@ import ua.onufreiv.hotel.entities.User;
  * Created by yurii on 12/29/16.
  */
 public interface IUserService {
+    User getById(int id);
     void registerNewUser(User user, PasswordHash pwdHash);
 }
--- src/main/java/ua/onufreiv/hotel/service/impl/UserService.java
+++ src/main/java/ua/onufreiv/hotel/service/impl/UserService.java
@@ -1,13 +1,19 @@
 package ua.onufreiv.hotel.service.impl;
 
+import ua.onufreiv.hotel.dao.DaoFactory;
+import ua.onufreiv.hotel.entities.PasswordHash;
 import ua.onufreiv.hotel.entities.User;
 import ua.onufreiv.hotel.service.IUserService;
-import ua.onufreiv.hotel.entities.PasswordHash;
 
 /**
  * Created by yurii on 12/29/16.
  */
 public class UserService implements IUserService {
+    @Override
+    public User getById(int id) {
+        return DaoFactory.getDAOFactory(DaoFactory.FactoryType.MYSQL_DB).getUserDao().find(id);
+    }
+
     @Override
     public void registerNewUser(User user, PasswordHash pwdHash) {
 
