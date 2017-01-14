0f227faf17bb139d76760732aa1b50f21c0abd00
Yurii <yurii.onufreiv@gmail.com>
2017-01-15 00:07:49 +0200

    idTypeTitlesMap attribute is set to the session

--- src/main/java/ua/onufreiv/hotel/controller/commands/CommandShowAdminDashboard.java
+++ src/main/java/ua/onufreiv/hotel/controller/commands/CommandShowAdminDashboard.java
@@ -2,13 +2,16 @@ package ua.onufreiv.hotel.controller.commands;
 
 import ua.onufreiv.hotel.controller.manager.PathConfig;
 import ua.onufreiv.hotel.entities.BookRequest;
+import ua.onufreiv.hotel.service.IRoomTypeService;
 import ua.onufreiv.hotel.service.impl.BookRequestService;
+import ua.onufreiv.hotel.service.impl.RoomTypeService;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by yurii on 1/5/17.
@@ -17,9 +20,12 @@ public class CommandShowAdminDashboard implements ua.onufreiv.hotel.controller.c
 
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        IRoomTypeService roomTypeService = new RoomTypeService();
         List<BookRequest> notProcessedRequests = new BookRequestService().getNotProcessedRequests();
         request.setAttribute("newBookRequests", notProcessedRequests);
         request.setAttribute("newBookRequestsCount", notProcessedRequests.size());
+        Map<Integer, String> idTypeTitleMap = roomTypeService.getIdTypeTitleMap();
+        request.getSession(false).setAttribute("idTypeTitlesMap", idTypeTitleMap);
         return PathConfig.getInstance().getProperty(PathConfig.ADMIN_DASHBOARD_PAGE_PATH);
     }
 }
