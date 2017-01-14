6dd787010a34c9a0a82e663d7b4ec9e56dc778d7
Yurii <yurii.onufreiv@gmail.com>
2017-01-14 22:55:11 +0200

    addNewRoomType() and containsType() methods in IRoomTypeService added

--- src/main/java/ua/onufreiv/hotel/service/IRoomTypeService.java
+++ src/main/java/ua/onufreiv/hotel/service/IRoomTypeService.java
@@ -8,6 +8,8 @@ import java.util.Map;
  * Created by yurii on 1/4/17.
  */
 public interface IRoomTypeService {
+    boolean addNewRoomType(RoomType roomType);
+    boolean containsType(String type);
     RoomType find(int id);
     Map<Integer, String> getIdTypeTitleMap();
     Map<Integer, RoomType> getIdTypeMap();
--- src/main/java/ua/onufreiv/hotel/service/impl/RoomTypeService.java
+++ src/main/java/ua/onufreiv/hotel/service/impl/RoomTypeService.java
@@ -4,12 +4,29 @@ import ua.onufreiv.hotel.dao.DaoFactory;
 import ua.onufreiv.hotel.entities.RoomType;
 import ua.onufreiv.hotel.service.IRoomTypeService;
 
+import java.util.List;
 import java.util.Map;
 
 /**
  * Created by yurii on 1/4/17.
  */
 public class RoomTypeService implements IRoomTypeService {
+    @Override
+    public boolean addNewRoomType(RoomType roomType) {
+        return DaoFactory.getDAOFactory(DaoFactory.FactoryType.MYSQL_DB).getRoomTypeDao().insert(roomType) > 0;
+    }
+
+    @Override
+    public boolean containsType(String type) {
+        List<String> allRoomTypes = DaoFactory.getDAOFactory(DaoFactory.FactoryType.MYSQL_DB).getRoomTypeDao().getAllRoomTypes();
+        for(String existingType : allRoomTypes) {
+            if(existingType.trim().equalsIgnoreCase(type.trim())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     @Override
     public RoomType find(int id) {
         return DaoFactory.getDAOFactory(DaoFactory.FactoryType.MYSQL_DB).getRoomTypeDao().find(id);
