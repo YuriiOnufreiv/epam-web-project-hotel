eb8bee5c9c1beb47eec645d7247542641e3aa7ad
Yurii <yurii.onufreiv@gmail.com>
2017-01-14 15:28:27 +0200

    Distinct 'bill info' pages for admin and client

--- src/main/java/ua/onufreiv/hotel/controller/manager/PathConfig.java
+++ src/main/java/ua/onufreiv/hotel/controller/manager/PathConfig.java
@@ -19,7 +19,8 @@ public class PathConfig {
     public static final String NOT_SIGNED_IN_ERROR_PAGE_PATH = "NOT_SIGNED_IN_ERROR_PAGE_PATH";
     public static final String ADMIN_DASHBOARD_PAGE_PATH = "ADMIN_DASHBOARD_PAGE_PATH";
     public static final String PROCESS_REQUEST_PAGE_PATH = "PROCESS_REQUEST_PAGE_PATH";
-    public static final String BILL_INFO_PAGE_PATH = "BILL_INFO_PAGE_PATH";
+    public static final String ADMIN_BILL_INFO_PAGE_PATH = "ADMIN_BILL_INFO_PAGE_PATH";
+    public static final String CLIENT_BILL_INFO_PAGE_PATH = "CLIENT_BILL_INFO_PAGE_PATH";
 
     private static PathConfig instance;
 
