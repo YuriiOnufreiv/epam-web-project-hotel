9b7e1aa84a8dafe3b20f011350c4514a305b4540
Yurii <yurii.onufreiv@gmail.com>
2017-01-17 12:54:06 +0200

    DATABASE_DRIVER_NAME and DATABASE_URL constants removed

--- src/main/java/ua/onufreiv/hotel/controller/manager/PathConfig.java
+++ src/main/java/ua/onufreiv/hotel/controller/manager/PathConfig.java
@@ -8,8 +8,6 @@ import java.util.ResourceBundle;
 public class PathConfig {
     private static final String BUNDLE_NAME = "paths";
 
-    public static final String DATABASE_DRIVER_NAME = "DATABASE_DRIVER_NAME";
-    public static final String DATABASE_URL = "DATABASE_URL";
     public static final String ERROR_PAGE_PATH = "ERROR_PAGE_PATH";
     public static final String MAIN_PAGE_PATH = "MAIN_PAGE_PATH";
     public static final String LOGIN_PAGE_PATH = "LOGIN_PAGE_PATH";
