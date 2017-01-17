993f05d32ad6750e403806bee3daf2fffe35e24e
Yurii <yurii.onufreiv@gmail.com>
2017-01-17 16:47:43 +0200

    MySqlUserDao uses custom QueryBuilder's

--- src/main/java/ua/onufreiv/hotel/persistence/dao/mysql/MySqlUserDao.java
+++ src/main/java/ua/onufreiv/hotel/persistence/dao/mysql/MySqlUserDao.java
@@ -4,8 +4,8 @@ import org.apache.log4j.Logger;
 import ua.onufreiv.hotel.entity.User;
 import ua.onufreiv.hotel.persistence.ConnectionManager;
 import ua.onufreiv.hotel.persistence.dao.IUserDao;
-import ua.onufreiv.hotel.persistence.jdbc.JdbcQuery;
-import ua.onufreiv.hotel.persistence.jdbc.query.QueryInsert;
+import ua.onufreiv.hotel.persistence.jdbc.query.QueryBuilder;
+import ua.onufreiv.hotel.persistence.jdbc.query.SqlQueryUpdate;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -27,15 +27,13 @@ public class MySqlUserDao implements IUserDao {
     private static final String COLUMN_PHONE_NUM_NAME = "phoneNum";
     private static final String COLUMN_ROLE_FK_NAME = "roleFK";
     private static final String COLUMN_PWD_FK_NAME = "pwdFK";
-    private static final String QUERY_INSERT = "INSERT INTO USER (name, surname, email, phoneNum, roleFK, pwdFK) VALUES (?, ?, ?, ?, ?, ?)";
-    private static final String QUERY_DELETE = "DELETE FROM USER WHERE idUser = ?";
-    private static final String QUERY_SELECT_BY_ID = "SELECT * FROM USER WHERE idUser = ?";
-    private static final String QUERY_SELECT_BY_EMAIL = "SELECT * FROM USER WHERE email = ?";
-    private static final String QUERY_SELECT_ALL = "SELECT * FROM USER";
-    private static final String QUERY_UPDATE = "UPDATE USER SET name = ?, surname = ?, email = ?, phoneNum = ?, roleFK = ?, pwdFK = ? WHERE idUser = ?";
+
     private static MySqlUserDao instance;
 
+    private QueryBuilder queryBuilder;
+
     private MySqlUserDao() {
+        queryBuilder = new QueryBuilder(TABLE_NAME);
     }
 
     public static synchronized MySqlUserDao getInstance() {
@@ -47,18 +45,17 @@ public class MySqlUserDao implements IUserDao {
 
     @Override
     public int insert(User user) {
-        int id = 0;
+        int id = -1;
         Connection connection = ConnectionManager.getConnection();
-        QueryInsert insert = new QueryInsert();
-        insert.into(TABLE_NAME)
-                .value(COLUMN_NAME_NAME, user.getName())
-                .value(COLUMN_SURNAME_NAME, user.getSurname())
-                .value(COLUMN_EMAIL_NAME, user.getEmail())
-                .value(COLUMN_PHONE_NUM_NAME, user.getPhoneNum())
-                .value(COLUMN_ROLE_FK_NAME, user.getUserRoleId())
-                .value(COLUMN_PWD_FK_NAME, user.getPwdHashId());
         try {
-            insert.execute(connection);
+            id = queryBuilder.insert()
+                    .value(COLUMN_NAME_NAME, user.getName())
+                    .value(COLUMN_SURNAME_NAME, user.getSurname())
+                    .value(COLUMN_EMAIL_NAME, user.getEmail())
+                    .value(COLUMN_PHONE_NUM_NAME, user.getPhoneNum())
+                    .value(COLUMN_ROLE_FK_NAME, user.getUserRoleId())
+                    .value(COLUMN_PWD_FK_NAME, user.getPwdHashId())
+                    .execute(connection);
         } catch (SQLException e) {
             logger.error("Failed to insert new user: ", e);
         } finally {
@@ -70,37 +67,47 @@ public class MySqlUserDao implements IUserDao {
     @Override
     public boolean delete(int id) {
         Connection connection = ConnectionManager.getConnection();
-        JdbcQuery jdbcQuery = new JdbcQuery();
-        boolean result = jdbcQuery.delete(connection, QUERY_DELETE, id);
-        ConnectionManager.closeConnection(connection);
+        boolean result = false;
+        try {
+            result = queryBuilder.wrapWithWhereClause(queryBuilder.delete())
+                    .addCondition().column(COLUMN_ID_NAME).isEqual(id)
+                    .executeUpdate(connection);
+        } catch (SQLException e) {
+            logger.error("Failed to delete user by id: ", e);
+        } finally {
+            ConnectionManager.closeConnection(connection);
+        }
         return result;
-        //return false;
     }
 
     @Override
     public User find(int id) {
         Connection connection = ConnectionManager.getConnection();
+        User user = null;
         try {
-            JdbcQuery jdbcQuery = new JdbcQuery();
-            ResultSet rs = jdbcQuery.select(connection, QUERY_SELECT_BY_ID, id);
+            ResultSet rs = queryBuilder.wrapWithWhereClause(queryBuilder.select())
+                    .addCondition().column(COLUMN_ID_NAME).isEqual(id)
+                    .executeQuery(connection);
             if (rs.next()) {
-                User user = DtoMapper.ResultSet.toUser(rs);
+                user = DtoMapper.ResultSet.toUser(rs);
                 ConnectionManager.closeConnection(connection);
                 return user;
             }
         } catch (SQLException e) {
             logger.error("Failed to find user by id: ", e);
+        } finally {
+            ConnectionManager.closeConnection(connection);
         }
-        return null;
+        return user;
     }
 
     @Override
     public List<User> findAll() {
         Connection connection = ConnectionManager.getConnection();
+        List<User> users = null;
         try {
-            JdbcQuery jdbcQuery = new JdbcQuery();
-            ResultSet rs = jdbcQuery.select(connection, QUERY_SELECT_ALL);
-            List<User> users = new ArrayList<>();
+            ResultSet rs = queryBuilder.select().execute(connection);
+            users = new ArrayList<>();
             while (rs.next()) {
                 users.add(DtoMapper.ResultSet.toUser(rs));
             }
@@ -108,40 +115,53 @@ public class MySqlUserDao implements IUserDao {
             return users;
         } catch (SQLException e) {
             logger.error("Failed to find all users: ", e);
+        } finally {
+            ConnectionManager.closeConnection(connection);
         }
-        return null;
+        return users;
     }
 
     @Override
     public boolean update(User user) {
+        boolean result = false;
         Connection connection = ConnectionManager.getConnection();
-        JdbcQuery jdbcQuery = new JdbcQuery();
-        boolean result = jdbcQuery.update(connection, QUERY_UPDATE,
-                user.getName(),
-                user.getSurname(),
-                user.getEmail(),
-                user.getPhoneNum(),
-                user.getUserRoleId(),
-                user.getPwdHashId(),
-                user.getId());
-        ConnectionManager.closeConnection(connection);
+        try {
+            SqlQueryUpdate queryUpdate = queryBuilder.update()
+                    .set(COLUMN_NAME_NAME, user.getName())
+                    .set(COLUMN_SURNAME_NAME, user.getSurname())
+                    .set(COLUMN_EMAIL_NAME, user.getEmail())
+                    .set(COLUMN_PHONE_NUM_NAME, user.getPhoneNum())
+                    .set(COLUMN_ROLE_FK_NAME, user.getUserRoleId())
+                    .set(COLUMN_PWD_FK_NAME, user.getPwdHashId());
+            result = queryBuilder.wrapWithWhereClause(queryUpdate)
+                    .addCondition().column(COLUMN_ID_NAME).isEqual(user.getId())
+                    .executeUpdate(connection);
+        } catch (SQLException e) {
+            logger.error("Failed to update user: ", e);
+        } finally {
+            ConnectionManager.closeConnection(connection);
+        }
         return result;
     }
 
     @Override
     public User find(String email) {
         Connection connection = ConnectionManager.getConnection();
+        User user = null;
         try {
-            JdbcQuery jdbcQuery = new JdbcQuery();
-            ResultSet rs = jdbcQuery.select(connection, QUERY_SELECT_BY_EMAIL, email);
+            ResultSet rs = queryBuilder.wrapWithWhereClause(queryBuilder.select())
+                    .addCondition().column(COLUMN_EMAIL_NAME).isEqual(email)
+                    .executeQuery(connection);
             if (rs != null && rs.next()) {
-                User user = DtoMapper.ResultSet.toUser(rs);
+                user = DtoMapper.ResultSet.toUser(rs);
                 ConnectionManager.closeConnection(connection);
                 return user;
             }
         } catch (SQLException e) {
             logger.error("Failed to find user by email: ", e);
+        } finally {
+            ConnectionManager.closeConnection(connection);
         }
-        return null;
+        return user;
     }
 }
