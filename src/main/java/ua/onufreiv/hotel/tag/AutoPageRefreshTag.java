cafa6c98d11366289cb591f6460a66d8dd60a21e
Yurii <yurii.onufreiv@gmail.com>
2017-01-27 13:51:26 +0200

    AutoPageRefreshTag created

---
+++ src/main/java/ua/onufreiv/hotel/tag/AutoPageRefreshTag.java
@@ -0,0 +1,25 @@
+package ua.onufreiv.hotel.tag;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.PageContext;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+import java.io.IOException;
+
+/**
+ * Created by yurii on 1/15/17.
+ */
+public class AutoPageRefreshTag extends SimpleTagSupport {
+    private int interval;
+
+    public void setInterval(int interval) {
+        this.interval = interval;
+    }
+
+    @Override
+    public void doTag() throws JspException, IOException {
+        PageContext context = (PageContext) getJspContext();
+        HttpServletResponse response = (HttpServletResponse) context.getResponse();
+        response.setIntHeader("Refresh", interval);
+    }
+}
